8a296feeedea68246675a776d2eb8fd0b87aaec4
Kanneka <bunthuonkanneka@gmail.com>
2022-07-05 23:27:03 +1000

    Initial Commit

---
+++ CoinToss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinToss
+//
+//  Created by Bunthuon Kanneka on 5/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
