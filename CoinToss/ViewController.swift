fb2d8f6ebb26ed867c8d41d748f9c42bbebc8ad7
Kanneka <bunthuonkanneka@gmail.com>
2022-07-06 01:01:50 +1000

    coin toss is tossing

--- CoinToss/ViewController.swift
+++ CoinToss/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBAction func tossButton(_ sender: Any) {
+        let randomInt = Int.random(in: 0..<2)
+        
+        if randomInt == 0 {
+            label.text = "H"
+        } else if randomInt == 1 {
+            label.text = "T"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
